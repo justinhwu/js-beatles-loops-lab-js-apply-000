ff44c38b1dc09948f9cc3926c6663f8fdda90a65
Justin Wu <justinhwu95@gmail.com>
2019-03-05 01:20:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts){
   let i = 0
   while(i<facts.length){
-    console.log(`${facts[i]}!!!`) 
+    console.log(`${facts[i]}!!!`)
+    i++
   }
 }
