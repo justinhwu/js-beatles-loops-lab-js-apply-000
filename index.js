6bffc43d42b4f7a563f766a8e0e9c2fcdc623c14
Justin Wu <justinhwu95@gmail.com>
2019-03-05 01:27:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function johnLennonFacts(facts){
 
 function iLoveTheBeatles(num){
   var array = []
-  var i =0
+  
   do{
     array.push("I love the Beatles!")
-    i++
+    num++
     }while(num<15)
     return array
 }
